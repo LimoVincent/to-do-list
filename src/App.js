ad8b7c8a67a3f11bffb377eeee06119994a64265
Limo Vincent <48591371+LimoVincent@users.noreply.github.com>
2020-05-10 21:33:23 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import ToDos from './components/ToDos';
 
-function App() {
-  return (
+
+ class App extends Component {
+  
+
+   state = {
+    todos : [{
+      id: 1, 
+      title: 'To do 1',
+      completed : false
+   },
+   {
+     id: 2, 
+    title: 'To do 2',
+    completed : false
+  },
+  {
+    id: 3, 
+    title: 'To do 3',
+    completed : false
+  }, 
+  {
+  id: 4, 
+  title: 'To do 4',
+  completed : false
+  }
+  ]
+  }
+  
+  render() {
+    
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <h1>ToDos : </h1>
+      <ToDos todos = {this.state.todos}/>
+      </div>
+    )
+  }
 }
-
 export default App;
+
+
+
---
+++ src/components/ToDos.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class ToDos extends Component {
+    render() {
+        return (
+            <div>
+                <h1>To ToDos</h1>
+            </div>
+        )
+    }
+}
+
+
